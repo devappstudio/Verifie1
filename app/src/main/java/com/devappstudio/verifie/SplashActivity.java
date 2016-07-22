c4a84eb49a39aa8dfa06ea41c8183aa10da9bb1c
hmbrainiac <isaacbramang@gmail.com>
2016-07-22 16:52:50 +0000

    close of friday progress

--- app/src/main/java/com/devappstudio/verifie/SplashActivity.java
+++ app/src/main/java/com/devappstudio/verifie/SplashActivity.java
@@ -1,7 +1,7 @@
 package com.devappstudio.verifie;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -11,6 +11,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
     }
 
     @Override
