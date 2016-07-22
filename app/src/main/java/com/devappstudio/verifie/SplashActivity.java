6a5c53c6f4bf2a5dd282593ccf6deba78891affa
hmbrainiac <isaacbramang@gmail.com>
2016-07-22 18:16:06 +0000

    close of friday progress

--- app/src/main/java/com/devappstudio/verifie/SplashActivity.java
+++ app/src/main/java/com/devappstudio/verifie/SplashActivity.java
@@ -1,23 +1,82 @@
 package com.devappstudio.verifie;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.Slide;
+import android.transition.TransitionInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setupWindowAnimations();
+
+        final Intent intent = new Intent(this, Login.class);
+        intent.putExtra("val", "shown");
+        final RequestQueue rq = Volley.newRequestQueue(getApplicationContext());
+        StringRequest sr = new StringRequest("http://rockradiogh.com/api_/audio.php",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String arg0) {
+                        //System.out.println(arg0);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                        intent.putExtra("link", arg0);
+                        rq.stop();
+                        startActivity(intent);
+                        finish();
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError arg0) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(), "Sorry no internet found",
+                        Toast.LENGTH_LONG).show();
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                intent.putExtra("link", arg0);
+                rq.stop();
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+        rq.add(sr);
+        rq.start();
 
     }
-
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void setupWindowAnimations() {
+        Slide slide = (Slide) TransitionInflater.from(this).inflateTransition(R.transition.activity_slide);
+        getWindow().setExitTransition(slide);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
         return true;
     }
 
@@ -26,13 +85,6 @@ public class SplashActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
