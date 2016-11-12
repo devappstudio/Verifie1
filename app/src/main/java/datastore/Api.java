b664efb8ad1746f3ffd37d7aaac7c47c8b3e038f
Isaac <isaacbremang@gmail.com>
2016-11-12 01:29:47 +0000

    Addition of cropping functionalities

--- app/src/main/java/datastore/Api.java
+++ app/src/main/java/datastore/Api.java
@@ -9,7 +9,7 @@ public class Api {
     public static final String api = "http://admin.verifieworld.com/index.php/api_v1/";
     public static final String image_end = "http://admin.verifieworld.com/index.php/auth/user_image/";
     // File upload url (replace the ip with your server address)
-//    public static final String FILE_UPLOAD_URL = "http://192.168.0.104/AndroidFileUpload/fileUpload.php";
+   public static final String FILE_UPLOAD_URL = "http://192.168.0.104/AndroidFileUpload/fileUpload.php";
 
     // Directory name to store captured images and videos
     public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
